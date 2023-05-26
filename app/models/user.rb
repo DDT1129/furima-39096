abdc9b99bde0926228530a45047c3f0aeaaebecb
DDT1129 <dai1129dt@gmail.com>
2023-05-27 00:00:09 +0900

    ユーザーモデルにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, format: {with: /\A/[ぁ-んァ-ヶ一-龥々ー]\z/}, presence: true
+  validates :last_name, format: {with: /\A/[ぁ-んァ-ヶ一-龥々ー]\z/}, presence: true
+  validates :first_name_kana, format: {with: /\A/[ァ-ヶ]\z/}, presence: true
+  validates :last_name_kana, format: {with: /\A/[ァ-ヶ]\z/}, presence: true
+  validates :birthday, presence: true
+
+  validates :encrypted_password, format: {with: /\A/[a-z\d]\z/}
+
+
 end
+
