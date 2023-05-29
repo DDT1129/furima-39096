8bb8564043e815bb9191aac56852f6e132bd3dfc
DDT1129 <dai1129dt@gmail.com>
2023-05-29 22:22:27 +0900

    Rubocop実行（コードの整理）

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,6 +1,4 @@
 class MarketsController < ApplicationController
-
   def index
   end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,
-    presence: true
+            presence: true
   validates :password,
-    format: { with: /\A[a-zA-Z\d]+\z/, message: '６文字以上の半角英数字混合で入力してください'}  
+            format: { with: /\A[a-zA-Z\d]+\z/, message: '６文字以上の半角英数字混合で入力してください' }
   with_options presence: true,
-    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を入力してください' } do
-      validates :first_name
-      validates :last_name
-    end
-         
+               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を入力してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
   with_options presence: true,
-    format: { with: /\A[ァ-ヴ]+\z/, message: '全角(カタカナ)文字を入力してください'} do
-      validates :first_name_kana
-      validates :last_name_kana
-    end
-         
+               format: { with: /\A[ァ-ヴ]+\z/, message: '全角(カタカナ)文字を入力してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
   validates :birthday, presence: true
-      
- 
-       
+
   has_many :order_recodes
   has_many :items
-
 end
-
