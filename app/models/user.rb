d37d7b0f9ea9bb865029d0b7f7e5e7a99b710ae2
DDT1129 <dai1129dt@gmail.com>
2023-05-29 22:09:42 +0900

    バリデーションの記述とエラーハンドリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ--一-龥々ー]\z/, message: '全角文字を入力してください' } do
-    validates :first_name
-    validates :last_name
-  end
-  
-  with_options presence: true, format: { with: /\A[ァ-ヶ--]\z/, message: '全角(カタカナ)文字を入力してください'} do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-  
+  validates :nickname,
+    presence: true
+  validates :password,
+    format: { with: /\A[a-zA-Z\d]+\z/, message: '６文字以上の半角英数字混合で入力してください'}  
+  with_options presence: true,
+    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を入力してください' } do
+      validates :first_name
+      validates :last_name
+    end
+         
+  with_options presence: true,
+    format: { with: /\A[ァ-ヴ]+\z/, message: '全角(カタカナ)文字を入力してください'} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+         
   validates :birthday, presence: true
-
-  validates :encrypted_password, format: { with: /\A[a-z\d]\z/, imessage: '英数字混合で入力してください'}
+      
+ 
+       
+  has_many :order_recodes
+  has_many :items
 
 end
 
