dcefee878b1eef266c31efe64982b2ecb7b5640b
DDT1129 <dai1129dt@gmail.com>
2023-06-01 19:56:06 +0900

    バリデーション、単体テストコード、マイグレーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname,
             presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '英字,数字の両方を含めて入力してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英数字の両方を含めて半角で入力してください'
   with_options presence: true,
                format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を入力してください' } do
     validates :first_name
@@ -15,13 +15,13 @@ class User < ApplicationRecord
   end
 
   with_options presence: true,
-               format: { with: /\A[ァ-ヴ]+\z/, message: '全角(カタカナ)文字を入力してください' } do
+               format: { with: /\A[ァ-ヶー]+\z/, message: '全角(カタカナ)文字を入力してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
   validates :birthday, presence: true
 
-  has_many :order_recodes
-  has_many :items
+  # has_many :order_recodes
+  # has_many :items
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,15 +13,6 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
       it '全項目(nickname,email,password,encrypted_password,first_name,first_name_kane,last_name,last_name_kana,birthday)を入力することで登録できる' do
-        @user.nickname = 'test'
-        @user.password = 'test123'
-        @user.password_confirmation = 'test123'
-        @user.email = 'test@test'
-        @user.first_name = 'テスト'
-        @user.first_name_kana = 'テスト'
-        @user.last_name = 'テスト'
-        @user.last_name_kana = 'テスト'
-        @user.birthday = '2023.05.31'
         expect(@user).to be_valid
       end
       it 'first_name,last_nameが全角であれば登録できる' do
@@ -77,17 +68,33 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it 'passwordが存在し、encrypted_passwordが空では登録できない' do
-        @user.password = 'fp1234'
+      it 'encrypted_passwordが空では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it 'passwordは、半角英数字混合でなければ登録できない' do
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank", "Password 英数字の両方を含めて半角で入力してください", "Password confirmation doesn't match Password")
+      end
+      it 'passwordは、英字のみのパスワードでは登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password 英字,数字の両方を含めて入力してください')
+        expect(@user.errors.full_messages).to include("Password 英数字の両方を含めて半角で入力してください")
+      end
+      it 'passwordは、数字のみのパスワードでは登録できない' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password 英数字の両方を含めて半角で入力してください")
+      end
+      it 'passwordは、全角文字を含むパスワードでは登録できない' do
+        @user.password = 'TEST１２３'
+        @user.password_confirmation = 'test１２３'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password 英数字の両方を含めて半角で入力してください")
       end
       it 'first_nameが半角では登録できない' do
         @user.first_name = 'ﾃｽﾄ'
@@ -98,6 +105,16 @@ RSpec.describe User, type: :model do
         @user.last_name = 'ﾃｽﾄ'
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name 全角文字を入力してください')
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank", "First name 全角文字を入力してください")
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank", "Last name 全角文字を入力してください")
       end
       it 'first_name_kanaが半角では登録できない' do
         @user.first_name_kana = 'ﾃｽﾄ'
