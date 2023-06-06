f7070ba861d17eb2a38cbe08390bfa231d30f63f
DDT1129 <dai1129dt@gmail.com>
2023-06-06 23:50:48 +0900

    ActiveHashの導入、アソシエーションとバリデーションの設定

---
+++ app/models/article.rb
@@ -0,0 +1,16 @@
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :detail
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :shipping_price
+  
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :detail_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :shipping_price_id
+  end
+end
