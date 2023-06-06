f92bc54ed79dd319d7594265c5c92989f7843558
DDT1129 <dai1129dt@gmail.com>
2023-06-06 15:01:40 +0900

    Active Storageの導入

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,16 @@
 class Market < ApplicationRecord
+  has_one_attached :image
+
+
+
+
+
   belongs_to :user
   has_one :order_recode
+
+  private
+
+  def message_params
+    params.require(:market).permit(:name, :explanation, :price, :category_id, :detail_id, :shipping_date_id, :prefecture_id, :user_id, :image).merge(user_id: current_user.id)
+  end
 end
