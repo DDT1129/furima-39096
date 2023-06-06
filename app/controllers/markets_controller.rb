947cb51a5c7b03d12216d6869087d5ad0b7e1902
DDT1129 <dai1129dt@gmail.com>
2023-06-06 16:08:30 +0900

    商品出品ページ遷移の制限設定（ログインの有無）

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,8 +1,21 @@
 class MarketsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
     @market = Market.new
+  end
+  
+  def create
+    
+  
+  end
+  
+  private
+
+  def market_params
+    params.require(:market).permit(:name, :explanation, :price, :category_id, :detail_id, :shipping_date_id, :prefecture_id, :user_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -7,10 +7,4 @@ class Market < ApplicationRecord
 
   belongs_to :user
   has_one :order_recode
-
-  private
-
-  def message_params
-    params.require(:market).permit(:name, :explanation, :price, :category_id, :detail_id, :shipping_date_id, :prefecture_id, :user_id, :image).merge(user_id: current_user.id)
-  end
 end
