1d3f462dc6381fe44b82f5dee69882f92b2f4f40
DDT1129 <dai1129dt@gmail.com>
2023-06-09 17:36:08 +0900

    不要な記述の削除

--- app/javascript/market_price.js
+++ app/javascript/market_price.js
@@ -6,12 +6,9 @@ window.addEventListener('load', () => {
 
   priceInput.addEventListener("input", (e) => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
       addPriceTax.innerHTML = (Math.floor(inputValue*0.1));
-      console.log(addPriceTax);
         const taxValue = inputValue * 0.1
         profitValue.innerHTML = (Math.floor(inputValue - taxValue));
-        console.log(profitValue);
   });
 });
 
