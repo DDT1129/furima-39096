27581c7edd38f78f961013850a52e57942680347
DDT1129 <dai1129dt@gmail.com>
2023-06-17 00:45:18 +0900

    orderのルート、バリデーション、アソシエーション、コントローラの修正とview表示（商品名等の登録商品の各項目）
    newアクションは不要であったため、ルートからアクションまで削除
    
    アソシエーションを組み忘れて、フォームから送られてくるパラメーターが空の状態になっていたため修正。
    
    ルボコップの実施

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,25 @@
 class OrdersController < ApplicationController
-  
   def index
     @market = Market.find(params[:market_id])
-  end
-
-  def new
     @order_address = OrderAddress.new
   end
 
   def create
+    @market = Market.find(params[:market_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
-
 
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_umber, :building, :phone).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_umber, :building, :phone).merge(
+      user_id: current_user.id, market_id: params[:market_id]
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root to: "markets#index"
   resources :markets do
-    resources :orders, only: :index
+    resources :orders, only: [:index, :create]
   end
 end
